3994eb0f0b36a934189d2d0db448a19657fcfdbb
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-27 02:02:42 +0600

    improved Queue class

--- src/com/azad/practice/java/try_this_projects/no_5_2/Queue.java
+++ src/com/azad/practice/java/try_this_projects/no_5_2/Queue.java
@@ -6,12 +6,37 @@ public class Queue {
 	private int putloc; // holds the position to put an element
 	private int getloc; // holds the position to get an element
 	
+	// construct an empty Queue given its size
 	public Queue(int size) {
 		q = new char[size];
 		putloc = 0;
 		getloc = 0;
 	}
 	
+	// construct a Queue with initial value
+	public Queue(char[] q) {
+		q = new char[q.length];
+		putloc = 0;
+		getloc = 0;
+		
+		for (int i = 0; i < q.length; i++) {
+			put(q[i]);
+		}
+	}
+	
+	// construct a Queue from a Queue
+	public Queue(Queue queue) {
+		q = new char[queue.q.length];
+		putloc = queue.putloc;
+		getloc = queue.getloc;
+		
+		// copy elements
+		for (int i = getloc; i < putloc; i++) {
+			q[i] = queue.q[i];
+		}
+	}
+	
+	// put a character into the Queue
 	public void put(char ch) {
 		if (putloc == size()) {
 			System.out.println(" - Queue is full.");
@@ -20,6 +45,7 @@ public class Queue {
 		q[putloc++] = ch;
 	}
 	
+	// get a character from the Queue
 	public char get() {
 		if (putloc == getloc) {
 			System.out.println(" - Queue is empty.");
@@ -28,10 +54,12 @@ public class Queue {
 		return q[getloc++];
 	}
 	
+	// get the Queue size/length
 	public int size() {
 		return q.length;
 	}
 	
+	// print the Queue
 	public void show() {
 		for (int i = 0; i < q.length; i++) {
 
